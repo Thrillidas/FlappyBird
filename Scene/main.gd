69d47b36eaa6e88b2e7538b3c501dd2a97e8ed6b
Thrillidas <franciscobrucesubiabre@gmail.com>
2024-08-21 17:22:44 -0400

    Third programming session

--- Scene/main.gd
+++ Scene/main.gd
@@ -1,14 +1,51 @@
 extends Node2D
-var pipe_node = preload("res://Scene/pipelines.tscn")
+var pipe_node  = preload("res://Scene/pipelines.tscn")
+var screen_max_height = DisplayServer.window_get_size()
 
 func _ready():
-	spawn_pipes()
-	spawn_pipes()
-	spawn_pipes()
-	
+	spawn_double_pipes(500)
+	spawn_double_pipes(500)
 
-func spawn_pipes():
+#Create a pipeline, allowing to specify the size and location.
+func spawn_pipes(x_size, y_size, x_position, y_position):
 	var pipe = pipe_node.instantiate()
-	pipe.position
+	pipe.create_pipe(x_size, y_size, x_position, y_position)
+	
 	add_child(pipe)
+	
+#Create a pair of pipeline, from the lower pipeline to the higher one.
+func spawn_double_pipes(pipe_location):
+	var random_height_size = randi() % 768 + 50
+	var gap = 100
+	var bottom_size = (screen_max_height.y - random_height_size) - gap
+	var bottom_height_position = screen_max_height.y - bottom_size
+	var pipe_width = 100
+	
+	spawn_pipes(pipe_width, random_height_size, pipe_location, 0)
+	spawn_pipes(pipe_width, bottom_size, pipe_location, bottom_height_position )
+
+
+##########################
+##This code doesnt work###
+##########################
+
+#extends Node2D
+#var pipe_node = preload("res://Scene/pipelines.tscn")
+#
+#func _ready():
+	#spawn_dual_pipes(100)
+	#spawn_dual_pipes(300)
+	#spawn_dual_pipes(500)
+	#
+#func spawn_pipes(x,y):
+	#var pipe = pipe_node.instantiate()
+	#pipe.position = Vector2(x,y)
+	#pipe.resize_pipe(50,200)
+	#add_child(pipe)
+	#
+#func spawn_dual_pipes(x):
+	#spawn_pipes(x,0)
+	#spawn_pipes(x,600)	
+
+
 	
--- Scripts/pipelines.gd
+++ Scripts/pipelines.gd
@@ -1,14 +1,26 @@
-extends StaticBody2D
+extends CharacterBody2D
 @onready var pipeline_sprite = $PipelineSprite
 
-func _ready():
-	grow()
-	
 func _physics_process(delta):
 	pass
+	#movement()
 	
-func grow():
-	var random_AxisY = randi()%-500 + 50
-	$PipelineSprite.size = Vector2(50,random_AxisY)
-	#print(random_AxisY)
+func create_pipe(x_size, y_size,x_position,y_position):
+	$PipelineSprite.size.x = x_size
+	$PipelineSprite.size.y = y_size
+	
+	$PipelineSprite.position.x = x_position
+	$PipelineSprite.position.y = y_position
+
+
+func resize_pipe(x_size, y_size):
+	$PipelineSprite.size.x = x_size
+	$PipelineSprite.size.y = y_size
+
+func movement():
+	velocity.x = -100
+	move_and_slide()
+	
+	
+
 	
