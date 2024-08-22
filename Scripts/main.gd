827b2839754d5da8f0c1c93de4c087d6d33d54ed
Thrillidas <franciscobrucesubiabre@gmail.com>
2024-08-22 12:33:00 -0400

    5th session

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -5,16 +5,12 @@ var screen_max_height = DisplayServer.window_get_size()
 func _ready():
 	spawn_double_pipes(500)
 	spawn_double_pipes(300)
-	
-	#spawn_pipes(100,150,500,0)
-	#spawn_pipes(100,150,300,668)
-	
-	pass
+		
 #Create a pipeline, allowing to specify the size and location.
 func spawn_pipes(x_size, y_size, x_position, y_position):
 	var pipe = pipe_node.instantiate()
-	pipe.create_pipe(x_size, y_size, x_position, y_position)
-	
+	pipe.create_pipe(x_size, y_size)
+	pipe.position = Vector2(x_position, y_position)
 	add_child(pipe)
 	
 #Create a pair of pipeline, from the lower pipeline to the higher one.
@@ -27,26 +23,5 @@ func spawn_double_pipes(pipe_location):
 	
 	spawn_pipes(pipe_width, random_height_size, pipe_location, 0)
 	spawn_pipes(pipe_width, bottom_size, pipe_location, bottom_height_position)
-	
-
-
-#extends Node2D
-#var pipe_node = preload("res://Scene/pipelines.tscn")
-#
-#func _ready():
-	#spawn_dual_pipes(100)
-	#spawn_dual_pipes(300)
-	#spawn_dual_pipes(500)
-	#
-#func spawn_pipes(x,y):
-	#var pipe = pipe_node.instantiate()
-	#pipe.position = Vector2(x,y)
-	#pipe.resize_pipe(50,200)
-	#add_child(pipe)
-	#
-#func spawn_dual_pipes(x):
-	#spawn_pipes(x,0)
-	#spawn_pipes(x,600)	
-
 
 	
--- Scripts/pipelines.gd
+++ Scripts/pipelines.gd
@@ -1,25 +1,16 @@
 extends CharacterBody2D
 @onready var pipeline_sprite = $PipelineSprite
-
+	
 func _physics_process(delta):
-	pass
-	#movement()
+	movement(delta)
 	
-func create_pipe(x_size, y_size,x_position,y_position):
-	$PipelineSprite.size.x = x_size
-	$PipelineSprite.size.y = y_size
+func create_pipe(x_size, y_size):
+	$PipelineSprite.size = Vector2(x_size,y_size)
 	
-	$PipelineSprite.position.x = x_position
-	$PipelineSprite.position.y = y_position
+func movement(delta):
+	velocity.x = -100
+	move_and_collide(velocity*delta)
 
-func resize_pipe(x_size, y_size):
-	$PipelineSprite.size.x = x_size
-	$PipelineSprite.size.y = y_size
-
-func movement():
-	velocity.x = 0
-	move_and_slide()
-	
 	
 
 	
