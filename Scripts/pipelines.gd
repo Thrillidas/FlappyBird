240600d53ef43c5b9f44c634e2a9ca38b7a0640f
Thrillidas <franciscobrucesubiabre@gmail.com>
2024-08-23 11:25:10 -0400

    Changes

--- Scripts/pipelines.gd
+++ Scripts/pipelines.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 @onready var pipeline_sprite = $PipelineSprite
-	
+@onready var collision_pipe = $CollisionPipe
+
 func _physics_process(delta):
 	movement(delta)
 	
 func create_pipe(x_size, y_size):
 	$PipelineSprite.size = Vector2(x_size,y_size)
+	$CollisionPipe.shape.size = Vector2(x_size,y_size)
 	
 func movement(delta):
 	velocity.x = -100
